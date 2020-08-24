163bd78b23e12d3b568dd591dd9fe786d1329017
sinsang <sw980321@naver.com>
2020-08-25 01:44:57 +0900

    20.08.25
    서버연동을 위한 DB수정

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   port: '3306',
-  user: 'root',
-  password: 'tlstkddn1',
-  database: 'dkni'
+  user: 'server',
+  password: 'tlstkddn!',
+  database: 'dksl_live'
 });
 
 connection.connect();
