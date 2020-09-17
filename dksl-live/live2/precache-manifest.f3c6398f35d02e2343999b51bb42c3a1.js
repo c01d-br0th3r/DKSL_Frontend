f14d116b89ec5047779fe9a66af3df3acb5f3f91
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-09-17 18:28:36 +0900

    :ambulance: Change to React.js
    react

---
+++ dksl-live/live2/precache-manifest.f3c6398f35d02e2343999b51bb42c3a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38885508f676bf85fca1e8c353008027",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d2f48fefb651adba7a4",
+    "url": "./static/js/2.982f9265.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "./static/js/2.982f9265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69fe9f19d20d68088d90",
+    "url": "./static/js/main.9eb914c2.chunk.js"
+  },
+  {
+    "revision": "bae20c8dc31699009b67",
+    "url": "./static/js/runtime-main.1f1936fb.js"
+  }
+]);
