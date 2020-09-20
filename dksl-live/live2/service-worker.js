2ce06a5601353381b787419b20b211dbd4348d29
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-09-20 23:50:11 +0900

    :rocket: Set Broadcast css
    css

--- dksl-live/live2/service-worker.js
+++ dksl-live/live2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d324cbaeb4fb36a4897758977d9212ec.js"
+  "/precache-manifest.3d524de1aa7d909364980879d5f45eac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
