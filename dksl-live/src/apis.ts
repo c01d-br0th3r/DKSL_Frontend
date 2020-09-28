d8eb5a592fa428dc00e38ae48a3cbc3bdb7c2dbe
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-09-28 16:24:03 +0900

    :goal_net: Make api module
    api

---
+++ dksl-live/src/apis.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://49.50.172.42:3001/",
+});
+
+const apis = {
+  fetchLeague: () => api.get(`getLeagues`),
+  fetchLeagueTeams: (id: number) => api.get(`getLeagueTeams/${id}`),
+  fetchTeamPlayers: (id: number) => api.get(`getTeamPlayers/${id}`),
+  fetchPlayerStat: (id: number) => api.get(`getPlayerStat/${id}`),
+};
+
+export default apis;
