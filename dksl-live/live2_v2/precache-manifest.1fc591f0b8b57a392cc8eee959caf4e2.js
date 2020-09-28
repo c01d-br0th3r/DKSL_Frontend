fd12b3c5d688b5815ff1bc3a65530b0f3d82a9f8
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-09-29 00:59:45 +0900

    :beers: Build version 2
    build

---
+++ dksl-live/live2_v2/precache-manifest.1fc591f0b8b57a392cc8eee959caf4e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7dc6f52e751cbe016aab45d1a0bf6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0f5dc1c40de3d0545d1",
+    "url": "/static/css/main.fdaef7b6.chunk.css"
+  },
+  {
+    "revision": "674db476cf2c9ac4c673",
+    "url": "/static/js/2.c1d28ed4.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.c1d28ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f5dc1c40de3d0545d1",
+    "url": "/static/js/main.f5d44ab6.chunk.js"
+  },
+  {
+    "revision": "f87f737d764c31ed97c4",
+    "url": "/static/js/runtime-main.c2e02797.js"
+  }
+]);
