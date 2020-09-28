5808fbd1b3f907f6af688a3b2145e225d43b71ee
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-09-28 16:23:35 +0900

    :globe_with_meridians: Make stat page router
    router

--- dksl-live/src/components/Router.tsx
+++ dksl-live/src/components/Router.tsx
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 import Main from "../routes/Main";
 import NotFound from "../routes/NotFound";
+import Stat from "../routes/Stat";
 
 export default () => (
   <>
     <Router>
       <Switch>
         <Route path="/live/:id" component={Main} />
+        <Route path="/stat/:id" component={Stat} />
         <Route path="/404" component={NotFound} />
         <Redirect path="*" to="/404" />
       </Switch>
