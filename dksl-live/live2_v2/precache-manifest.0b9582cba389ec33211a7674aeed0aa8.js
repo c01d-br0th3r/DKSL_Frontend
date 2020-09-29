96582386ce00a6ef8833274f21d6ece5c8b76634
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-09-29 15:03:54 +0900

    :ambulance: Build version 2
    build

---
+++ dksl-live/live2_v2/precache-manifest.0b9582cba389ec33211a7674aeed0aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fd3dbe31b7b88c930d0b5e276ccea3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b792e813cd199bd739fd",
+    "url": "/static/css/main.fdaef7b6.chunk.css"
+  },
+  {
+    "revision": "8226fe298afb8dabcac0",
+    "url": "/static/js/2.52943320.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.52943320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b792e813cd199bd739fd",
+    "url": "/static/js/main.933458c7.chunk.js"
+  },
+  {
+    "revision": "f87f737d764c31ed97c4",
+    "url": "/static/js/runtime-main.c2e02797.js"
+  }
+]);
