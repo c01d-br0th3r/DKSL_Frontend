1310fb39d8c004f9a1594b554e80dc973284f91a
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-10-03 00:09:43 +0900

    :pencil: Make Home pages with Container-Presenter Pattern
    home

---
+++ dksl-live/src/routes/Home/HomeContainer.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import HomePresenter from "./HomePresenter";
+
+const HomeContainer: React.FC<{}> = () => {
+  return (
+    <div>
+      <HomePresenter />
+    </div>
+  );
+};
+
+export default HomeContainer;
---
+++ dksl-live/src/routes/Home/HomePresenter.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../../components/Header";
+
+const HomePresenter: React.FC<{}> = () => {
+  return (
+    <div>
+      <Header />
+      <div>Home</div>
+    </div>
+  );
+};
+
+export default HomePresenter;
