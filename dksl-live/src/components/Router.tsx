4f9bc5b185302578fd779995d39c9195889797eb
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-10-03 03:07:36 +0900

    :globe_with_meridians: Make router for pages
    router

--- dksl-live/src/components/Router.tsx
+++ dksl-live/src/components/Router.tsx
@@ -9,14 +9,18 @@ import Home from "../routes/Home";
 import Main from "../routes/Main";
 import NotFound from "../routes/NotFound";
 import Stat from "../routes/Stat";
+import Search from "../routes/Search";
+import Header from "./Header";
 
 export default () => (
   <>
     <Router>
+      <Header />
       <Switch>
-        <Route exact path="/home" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/live/:id" component={Main} />
         <Route path="/stat/:id" component={Stat} />
+        <Route exact path="/search" component={Search} />
         <Route path="/404" component={NotFound} />
         <Redirect path="*" to="/404" />
       </Switch>
