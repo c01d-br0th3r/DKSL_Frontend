7cd7f515e15bf949d426edc705afba539413934b
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-10-03 03:04:55 +0900

    :fire: Make global header
    header

--- dksl-live/src/components/App.tsx
+++ dksl-live/src/components/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Router from "./Router";
 import GlobalStyle from "./GlobalStyle";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App: React.FC<{}> = () => {
   return (
