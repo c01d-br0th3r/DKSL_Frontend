1f64d6fb9f57cdfef858e9ffc98209cf0dc21f88
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-10-03 03:08:25 +0900

    :ambulance: Build live version 2
    build

---
+++ dksl-live/live2_v2/precache-manifest.1cf5508d5a83574b91f1c71847e490a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1719331ccd8155ecc2d28b2de0f3ce51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac0d1b24f4a1af14ae28",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "46c28d7b16ae68a4b8a8",
+    "url": "/static/css/main.fdaef7b6.chunk.css"
+  },
+  {
+    "revision": "ac0d1b24f4a1af14ae28",
+    "url": "/static/js/2.3bc1ada2.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.3bc1ada2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c28d7b16ae68a4b8a8",
+    "url": "/static/js/main.83cf4898.chunk.js"
+  },
+  {
+    "revision": "f87f737d764c31ed97c4",
+    "url": "/static/js/runtime-main.c2e02797.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- dksl-live/live2_v2/service-worker.js
+++ dksl-live/live2_v2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97bc0adaa322304ccdc8de9bfd8f004a.js"
+  "/precache-manifest.1cf5508d5a83574b91f1c71847e490a6.js"
 );
 
 self.addEventListener('message', (event) => {
