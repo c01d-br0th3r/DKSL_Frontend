44beda9e882d52dbaccaa475ee6fd29f8c294b12
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-10-03 03:04:27 +0900

    :construction: Append players api
    append

--- dksl-live/src/apis.ts
+++ dksl-live/src/apis.ts
@@ -4,12 +4,18 @@ const api = axios.create({
   baseURL: "http://49.50.172.42:3001/",
 });
 
+const socketServer = axios.create({
+  baseURL: "http://49.50.172.42:3002/",
+});
+
 const apis = {
   fetchLeague: () => api.get(`getLeagues`),
   fetchLeagueTeams: (id: number) => api.get(`getLeagueTeams/${id}`),
   fetchTeamPlayers: (id: number) => api.get(`getTeamPlayers/${id}`),
   fetchPlayerStat: (id: number) => api.get(`getPlayerStat/${id}`),
   fetchTeamInfo: (id: number) => api.get(`getTeamInfo/${id}`),
+  fetchAllPlayers: () => api.get(`getPlayerInfo`),
+  fetchLiveGames: () => socketServer.get(`getLiveGames`),
 };
 
 export default apis;
