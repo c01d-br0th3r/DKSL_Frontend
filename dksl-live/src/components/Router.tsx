b1f8f5ffea1ef0f7a062fc9bf1750f1ba8a15e1c
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-10-03 00:08:57 +0900

    :pushpin: Make home router
    make

--- dksl-live/src/components/Router.tsx
+++ dksl-live/src/components/Router.tsx
@@ -5,6 +5,7 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
+import Home from "../routes/Home";
 import Main from "../routes/Main";
 import NotFound from "../routes/NotFound";
 import Stat from "../routes/Stat";
@@ -13,6 +14,7 @@ export default () => (
   <>
     <Router>
       <Switch>
+        <Route exact path="/home" component={Home} />
         <Route path="/live/:id" component={Main} />
         <Route path="/stat/:id" component={Stat} />
         <Route path="/404" component={NotFound} />
