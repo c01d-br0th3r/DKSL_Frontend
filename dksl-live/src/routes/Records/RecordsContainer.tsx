813b7bfb2235d174890ed226a9d1e34e0814eb48
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-10-08 18:50:38 +0900

    :construction: Make Records directory (in progress)
    records

--- dksl-live/src/routes/Records/RecordsContainer.tsx
+++ dksl-live/src/routes/Records/RecordsContainer.tsx
@@ -1,7 +1,22 @@
-import React from "react";
-import RecordsPresenter from "./RecordPresenter";
+import React, { useState, useEffect } from "react";
+import apis from "../../apis";
+import RecordsPresenter from "./RecordsPresenter";
+import { IPlayerInfo } from "../../interfaces/stat";
 
 const RecordsContainer: React.FC<{}> = () => {
+  useEffect(() => {
+    const fetchAllPlayersStat = async () => {
+      try {
+        const { data: batter } = await apis.fetchAllPlayersBatterStat();
+        const { data: pitcher } = await apis.fetchAllPlayersPithcherStat();
+        console.log(batter);
+        console.log(pitcher);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchAllPlayersStat();
+  }, []);
   return (
     <div>
       <RecordsPresenter />
---
+++ dksl-live/src/routes/Records/RecordsPresenter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecordsPresenter: React.FC<{}> = () => {
+  return <div>REcords</div>;
+};
+
+export default RecordsPresenter;
