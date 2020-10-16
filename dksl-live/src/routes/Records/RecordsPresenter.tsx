b9d4dfcd294a89e3476eafdba681711c3afe53e9
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-10-17 03:01:59 +0900

    :bookmark: Change to reusable table component
    component

--- dksl-live/src/routes/Records/RecordsPresenter.tsx
+++ dksl-live/src/routes/Records/RecordsPresenter.tsx
@@ -2,6 +2,8 @@ import React, { ChangeEvent } from "react";
 import { IBatterStatObj, IPitcherObj } from "../../interfaces/stat";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import BatterTable from "../../components/BatterTable";
+import PitcherTable from "../../components/PitcherTable";
 
 interface IPresenter {
   allYears: number[];
@@ -55,44 +57,6 @@ const Pitcher: any = styled.div`
   color: ${(props: any) => (props.isOn ? "#2450aa" : "#000")};
 `;
 
-const TableWrapper = styled.div`
-  width: 100%;
-  overflow: scroll;
-  margin-bottom: 10px;
-`;
-
-const Table = styled.table`
-  width: 100%;
-  td {
-    text-align: center;
-    padding: 10px 15px;
-    padding-top: 12px;
-    white-space: nowrap;
-    font-weight: 500;
-  }
-  tr:nth-child(2n + 1) {
-    background-color: #f4f4f4;
-  }
-  tr:nth-child(1) {
-    background-color: #2450aa;
-    color: #fff;
-    font-weight: 600;
-  }
-`;
-
-const Rank = styled.td`
-  color: ${(props) =>
-    props.id === "1"
-      ? "#C3A935"
-      : props.id === "2"
-      ? "#BABABA"
-      : props.id === "3"
-      ? "#C77B30"
-      : "#000"};
-  font-weight: ${(props) =>
-    props.id === "1" || props.id === "2" || props.id === "3" ? "700" : "400"};
-`;
-
 const RecordsPresenter: React.FC<IPresenter> = ({
   allYears,
   year,
@@ -125,139 +89,15 @@ const RecordsPresenter: React.FC<IPresenter> = ({
       </div>
       <div>
         {isBatter ? (
-          <TableWrapper>
-            <Table>
-              <tbody>
-                <tr onClick={handleBatterKey}>
-                  <td>랭킹</td>
-                  <td>선수명</td>
-                  <td>AVG</td>
-                  <td>G</td>
-                  <td>PA</td>
-                  <td>AB</td>
-                  <td>R</td>
-                  <td>H</td>
-                  <td>2B</td>
-                  <td>3B</td>
-                  <td>HR</td>
-                  <td>TB</td>
-                  <td>RBI</td>
-                  <td>SB</td>
-                  <td>CS</td>
-                  <td>SAC</td>
-                  <td>SF</td>
-                  <td>BB</td>
-                  <td>IBB</td>
-                  <td>HBP</td>
-                  <td>SO</td>
-                  <td>GDP</td>
-                  <td>SLG</td>
-                  <td>OBP</td>
-                  <td>OPS</td>
-                </tr>
-                {batters.map((b, i) => {
-                  if (i < 40) {
-                    return (
-                      <tr key={b.playerId}>
-                        <Rank id={`${i + 1}`}>{i + 1}</Rank>
-                        <td>
-                          <Link to={`/stat/${b.playerId}`}>{b.playerName}</Link>
-                        </td>
-                        <td>{b.AVG && b.AVG.toFixed(3)}</td>
-                        <td>{b.G}</td>
-                        <td>{b.PA}</td>
-                        <td>{b.AB}</td>
-                        <td>{b.R}</td>
-                        <td>{b.H}</td>
-                        <td>{b._2B}</td>
-                        <td>{b._3B}</td>
-                        <td>{b.HR}</td>
-                        <td>{b.TB}</td>
-                        <td>{b.RBI}</td>
-                        <td>{b.SB}</td>
-                        <td>{b.CS}</td>
-                        <td>{b.SAC}</td>
-                        <td>{b.SF}</td>
-                        <td>{b.BB}</td>
-                        <td>{b.IBB}</td>
-                        <td>{b.HBP}</td>
-                        <td>{b.SO}</td>
-                        <td>{b.GDP}</td>
-                        <td>{b.SLG && b.SLG.toFixed(3)}</td>
-                        <td>{b.OBP && b.OBP.toFixed(3)}</td>
-                        <td>{b.OPS && b.OPS.toFixed(3)}</td>
-                      </tr>
-                    );
-                  }
-                })}
-              </tbody>
-            </Table>
-          </TableWrapper>
+          <BatterTable
+            batters={batters.slice(0, 40)}
+            handleClick={handleBatterKey}
+          />
         ) : (
-          <TableWrapper>
-            <Table>
-              <tbody>
-                <tr onClick={handlePitcherKey}>
-                  <td>랭킹</td>
-                  <td>선수명</td>
-                  <td>ERA</td>
-                  <td>G</td>
-                  <td>W</td>
-                  <td>L</td>
-                  <td>HLD</td>
-                  <td>SV</td>
-                  <td>IP</td>
-                  <td>H</td>
-                  <td>AB</td>
-                  <td>BB</td>
-                  <td>BK</td>
-                  <td>Batters</td>
-                  <td>ER</td>
-                  <td>HBP</td>
-                  <td>HR</td>
-                  <td>K</td>
-                  <td>NP</td>
-                  <td>R</td>
-                  <td>SAC</td>
-                  <td>SF</td>
-                  <td>WP</td>
-                </tr>
-                {pitchers.map((p, i) => {
-                  if (i < 40) {
-                    return (
-                      <tr key={p.playerId}>
-                        <Rank id={`${i + 1}`}>{i + 1}</Rank>
-                        <td>
-                          <Link to={`/stat/${p.playerId}`}>{p.playerName}</Link>
-                        </td>
-                        <td>{p.ERA && p.ERA.toFixed(2)}</td>
-                        <td>{p.G}</td>
-                        <td>{p.W}</td>
-                        <td>{p.L}</td>
-                        <td>{p.HLD}</td>
-                        <td>{p.SV}</td>
-                        <td>{p.IP && Math.floor(p.IP / 3)}</td>
-                        <td>{p.H}</td>
-                        <td>{p.AB}</td>
-                        <td>{p.BB}</td>
-                        <td>{p.BK}</td>
-                        <td>{p.Batters}</td>
-                        <td>{p.ER}</td>
-                        <td>{p.HBP}</td>
-                        <td>{p.HR}</td>
-                        <td>{p.K}</td>
-                        <td>{p.NP}</td>
-                        <td>{p.R}</td>
-                        <td>{p.SAC}</td>
-                        <td>{p.SF}</td>
-                        <td>{p.WP}</td>
-                      </tr>
-                    );
-                  }
-                })}
-              </tbody>
-            </Table>
-          </TableWrapper>
+          <PitcherTable
+            pitchers={pitchers.slice(0, 40)}
+            handleClick={handlePitcherKey}
+          />
         )}
       </div>
       <Notice>상위 40등까지 조회할 수 있습니다.</Notice>
--- dksl-live/src/routes/Teams/TeamsPresenter.tsx
+++ dksl-live/src/routes/Teams/TeamsPresenter.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { IBatterStatObj, IPitcherObj } from "../../interfaces/stat";
 import { ITeamInfo } from "../../interfaces/team";
-import { Link } from "react-router-dom";
+import BatterTable from "../../components/BatterTable";
+import PitcherTable from "../../components/PitcherTable";
 
 interface IProps {
   teamInfo: ITeamInfo;
@@ -36,44 +37,6 @@ const Pitcher: any = styled.div`
   align-items: center;
   cursor: pointer;
   color: ${(props: any) => (props.isOn ? "#2450aa" : "#000")};
-`;
-
-const TableWrapper = styled.div`
-  width: 100%;
-  overflow: scroll;
-  margin-bottom: 10px;
-`;
-
-const Table = styled.table`
-  width: 100%;
-  td {
-    text-align: center;
-    padding: 10px 15px;
-    padding-top: 12px;
-    white-space: nowrap;
-    font-weight: 500;
-  }
-  tr:nth-child(2n + 1) {
-    background-color: #f4f4f4;
-  }
-  tr:nth-child(1) {
-    background-color: #2450aa;
-    color: #fff;
-    font-weight: 600;
-  }
-`;
-
-const Rank = styled.td`
-  color: ${(props) =>
-    props.id === "1"
-      ? "#C3A935"
-      : props.id === "2"
-      ? "#BABABA"
-      : props.id === "3"
-      ? "#C77B30"
-      : "#000"};
-  font-weight: ${(props) =>
-    props.id === "1" || props.id === "2" || props.id === "3" ? "700" : "400"};
 `;
 
 const Team = styled.div`
@@ -119,131 +82,9 @@ const TeamsPresenter: React.FC<IProps> = ({
         </div>
         <div>
           {isBatter ? (
-            <TableWrapper>
-              <Table>
-                <tbody>
-                  <tr>
-                    <td>랭킹</td>
-                    <td>선수명</td>
-                    <td>AVG</td>
-                    <td>G</td>
-                    <td>PA</td>
-                    <td>AB</td>
-                    <td>R</td>
-                    <td>H</td>
-                    <td>2B</td>
-                    <td>3B</td>
-                    <td>HR</td>
-                    <td>TB</td>
-                    <td>RBI</td>
-                    <td>SB</td>
-                    <td>CS</td>
-                    <td>SAC</td>
-                    <td>SF</td>
-                    <td>BB</td>
-                    <td>IBB</td>
-                    <td>HBP</td>
-                    <td>SO</td>
-                    <td>GDP</td>
-                    <td>SLG</td>
-                    <td>OBP</td>
-                    <td>OPS</td>
-                  </tr>
-                  {batters.map((b, i) => (
-                    <tr key={b.playerId}>
-                      <Rank id={`${i + 1}`}>{i + 1}</Rank>
-                      <td>
-                        <Link to={`/stat/${b.playerId}`}>{b.playerName}</Link>
-                      </td>
-                      <td>{b.AVG && b.AVG.toFixed(3)}</td>
-                      <td>{b.G}</td>
-                      <td>{b.PA}</td>
-                      <td>{b.AB}</td>
-                      <td>{b.R}</td>
-                      <td>{b.H}</td>
-                      <td>{b._2B}</td>
-                      <td>{b._3B}</td>
-                      <td>{b.HR}</td>
-                      <td>{b.TB}</td>
-                      <td>{b.RBI}</td>
-                      <td>{b.SB}</td>
-                      <td>{b.CS}</td>
-                      <td>{b.SAC}</td>
-                      <td>{b.SF}</td>
-                      <td>{b.BB}</td>
-                      <td>{b.IBB}</td>
-                      <td>{b.HBP}</td>
-                      <td>{b.SO}</td>
-                      <td>{b.GDP}</td>
-                      <td>{b.SLG && b.SLG.toFixed(3)}</td>
-                      <td>{b.OBP && b.OBP.toFixed(3)}</td>
-                      <td>{b.OPS && b.OPS.toFixed(3)}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </TableWrapper>
+            <BatterTable batters={batters} />
           ) : (
-            <TableWrapper>
-              <Table>
-                <tbody>
-                  <tr>
-                    <td>랭킹</td>
-                    <td>선수명</td>
-                    <td>ERA</td>
-                    <td>G</td>
-                    <td>W</td>
-                    <td>L</td>
-                    <td>HLD</td>
-                    <td>SV</td>
-                    <td>IP</td>
-                    <td>H</td>
-                    <td>AB</td>
-                    <td>BB</td>
-                    <td>BK</td>
-                    <td>Batters</td>
-                    <td>ER</td>
-                    <td>HBP</td>
-                    <td>HR</td>
-                    <td>K</td>
-                    <td>NP</td>
-                    <td>R</td>
-                    <td>SAC</td>
-                    <td>SF</td>
-                    <td>WP</td>
-                  </tr>
-                  {pitchers.map((p, i) => (
-                    <tr key={p.playerId}>
-                      <Rank id={`${i + 1}`}>{i + 1}</Rank>
-                      <td>
-                        <Link to={`/stat/${p.playerId}`}>{p.playerName}</Link>
-                      </td>
-                      <td>{p.ERA && p.ERA.toFixed(2)}</td>
-                      <td>{p.G}</td>
-                      <td>{p.W}</td>
-                      <td>{p.L}</td>
-                      <td>{p.HLD}</td>
-                      <td>{p.SV}</td>
-                      <td>{p.IP && Math.floor(p.IP / 3)}</td>
-                      <td>{p.H}</td>
-                      <td>{p.AB}</td>
-                      <td>{p.BB}</td>
-                      <td>{p.BK}</td>
-                      <td>{p.Batters}</td>
-                      <td>{p.ER}</td>
-                      <td>{p.HBP}</td>
-                      <td>{p.HR}</td>
-                      <td>{p.K}</td>
-                      <td>{p.NP}</td>
-                      <td>{p.R}</td>
-                      <td>{p.SAC}</td>
-                      <td>{p.SF}</td>
-                      <td>{p.WP}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </TableWrapper>
+            <PitcherTable pitchers={pitchers} />
           )}
         </div>
       </div>
